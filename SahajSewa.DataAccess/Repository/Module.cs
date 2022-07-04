e0e21d2ea65205fead39e7741b653b66d9ad49f3
Rabi prajapati <cvile.last8@gmail.com>
2022-07-04 17:41:57 +0545

    Add project files.

---
+++ SahajSewa.DataAccess/Repository/Module.cs
@@ -0,0 +1,23 @@
+﻿using SahajSewa.DataAccess.Data;
+using SahajSewa.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SahajSewa.DataAccess.Repository
+{
+    public class Module : IModule
+    {
+        private readonly ApplicationDbContext _db;
+        public Module(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
