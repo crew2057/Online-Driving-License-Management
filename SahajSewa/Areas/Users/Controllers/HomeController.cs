bd7becf6d5ba745db99d6bb9ad44b5fea261d750
Rabi prajapati <cvile.last8@gmail.com>
2022-07-06 15:20:26 +0545

    data entered 1

--- SahajSewa/Areas/Users/Controllers/HomeController.cs
+++ SahajSewa/Areas/Users/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace SahajSewa.Controllers
             return View();
         }
 
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
