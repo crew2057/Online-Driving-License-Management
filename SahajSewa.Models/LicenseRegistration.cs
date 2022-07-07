e7081399dbbb69ad4d860f029477f83232688349
Rabi prajapati <cvile.last8@gmail.com>
2022-07-07 17:05:39 +0545

    license Registration

---
+++ SahajSewa.Models/LicenseRegistration.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SahajSewa.Models
+{
+    public class LicenseRegistration
+    {
+
+        #region General Details
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string Fname { get; set; }
+        [Display(Name = "Middle Name")]
+        public string Mname { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string Pnumber { get; set; }
+        [Required]
+        [Display(Name = "Blood Group")]
+        public string Bgroup { get; set; }
+        [Required]
+        [Display(Name = "Guardian Name")]
+        public string Gname { get; set; }
+        #endregion
+
+        #region Address Details
+        //permanent
+        [Required]
+        [Display(Name = "Province")]
+        public string Pprovince { get; set; }
+        [ForeignKey("Pprovince")]
+        public Province Province { get; set; }
+        [Required]
+        [Display(Name = "District")]
+        public string Pdistrict { get; set; }
+        [ForeignKey("Pdistrict")]
+        public District District { get; set; }
+        [Required]
+        [Display(Name = "Village / Municipality")]
+        public string Pvillage { get; set; }
+        [ForeignKey("Pvillage")]
+        public Village Village { get; set; }
+        [Required]
+        [Display(Name = "Ward No.")]
+        public int Pward { get; set; }
+        [Required]
+        [Display(Name = "Tole")]
+        public string Ptole { get; set; }
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        //temporary
+        [Required]
+        [Display(Name = "Province")]
+        public string Tprovince { get; set; }
+        [Required]
+        [Display(Name = "District")]
+        public string Tdistrict { get; set; }
+        [Required]
+        [Display(Name = "Village / Municipality")]
+        public string Tvillage { get; set; }
+        [Required]
+        [Display(Name = "Ward No.")]
+        public int Tward { get; set; }
+        [Required]
+        [Display(Name = "Tole")]
+        public string Ttole { get; set; }
+        #endregion
+
+        #region Citizenship Details
+        [Required]
+        [Display(Name ="Citizenship No")]
+        public string CitizenNo { get; set; }
+        [Required]
+        [Display(Name ="Citizenship Issue District")]
+        public string CitizenDistrict { get; set; }
+
+        [Required]
+        [Display(Name ="Citizenship Issue Date")]
+        public DateTime CitizenDate { get; set; }
+        #endregion
+
+        #region Exam Details
+        [Required]
+        [Display(Name ="Office Province")]
+        public string OfficeProvince { get; set; }
+        [Required]
+        [Display(Name ="Office")]
+        public string OfficeId { get; set; }
+        [ForeignKey("OfficeId")]
+        public Office Office { get; set; }
+        [Required]
+        public string Category { get; set; }
+        #endregion
+
+        #region Documents Upload
+        [Required]
+        [Display(Name ="Applicant's Photo")]
+        public string Photo { get; set; }
+        [Required]
+        [Display(Name ="Citizenship Front Photo")]
+        public string CitizenFront { get; set; }
+        [Required]
+        [Display(Name ="Citizenship Back Photo")]
+        public string CitizenBack { get; set; }
+        [Required]
+        [Display(Name ="Signature Photo")]
+        public string Signature { get; set; }
+        [Required]
+        [Display(Name ="Thumb Impression")]
+        public string Thumb { get; set; }
+        #endregion
+    }
+}
---
+++ SahajSewa/Areas/Users/Controllers/LicenseRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SahajSewa.Areas.Users.Controllers
+{
+    public class LicenseRegistration : Controller
+    {
+        //Get
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+    }
+}
