a83f5cd08bd5a73a95a59e4a0818f1c183a9fd4e
Rabi prajapati <cvile.last8@gmail.com>
2022-07-08 11:20:09 +0545

    updated

---
+++ SahajSewa/Areas/Users/Controllers/LicenseRegistrationController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SahajSewa.DataAccess.Data;
+using SahajSewa.DataAccess.Repository.IRepository;
+using SahajSewa.Models.ViewModels;
+
+namespace SahajSewa.Areas.Users.Controllers
+{
+    public class LicenseRegistrationController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IModule _module;
+        private readonly ApplicationDbContext _db;
+
+        public LicenseRegistrationController(
+            ApplicationDbContext db,
+            IModule module,
+            IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            _module = module;
+            _hostEnvironment = hostEnvironment;
+        }
+        //Get
+        public IActionResult Create()
+        {
+            LicenseRegistrationVM VM = new()
+            {
+                LicenseRegistration = new(),
+                ProvinceList = _db.Provinces.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                DistrictList = _db.Districts.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                VillageList = _db.Villages.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                OfficeList = _db.Offices.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+            };
+            return View(VM);
+        }
+
+
+    }
+}
