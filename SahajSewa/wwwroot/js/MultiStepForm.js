8d3e15bf15345c96b5f4f00b6dbeba75f9450436
Rabi prajapati <cvile.last8@gmail.com>
2022-07-08 19:37:29 +0545

    multi-step form

--- SahajSewa/wwwroot/js/MultiStepForm.js
+++ SahajSewa/wwwroot/js/MultiStepForm.js
@@ -1,1 +1,37 @@
-﻿
+﻿var next_click = document.querySelectorAll(".next_button");
+var main_form = document.querySelectorAll(".main");
+var step_list = document.querySelectorAll(".progres-bar li");
+let formnumber = 0;
+
+next_click.forEach(function (next_click_form) {
+    next_click_form.addEventListener('click', function () {
+        formnumber++;
+        updateform();
+        progress_forward();
+    });
+});
+
+var back_click = document.querySelectorAll(".back_button");
+back_click.forEach(function (back_click_form) {
+    back_click_form.addEventListener("click", function () {
+        formnumber--;
+        updateform();
+        progress_backward();
+    });
+});
+
+function updateform() {
+    main_form.forEach(function (mainform_number) {
+        mainform_number.classList.remove('activate');
+    });
+    main_form[formnumber].classList.add('activate');
+}
+
+function progress_forward() {
+    step_list[formnumber].classList.add('active');
+}
+
+function progress_backward() {
+    var form_num = formnumber + 1;
+    step_list[form_num].classList.remove('active');
+}
